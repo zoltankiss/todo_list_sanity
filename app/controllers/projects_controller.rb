58601ce05cf1e5758f1fdce2eb3d58e29d22b903
Zoltan Kiss <zkiss@ackmanndickenson.com>
2014-09-10 19:35:45 -0500

    redirect projects#show -> tasks#index

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -11,6 +11,7 @@ class ProjectsController < ApplicationController
   # GET /projects/1
   # GET /projects/1.json
   def show
+    redirect_to project_tasks_path(@project)
   end
 
   # GET /projects/new
